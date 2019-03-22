b14f90eed5f627a6c31e5be5a5c7694794e00294
Adam <alfredsson.adam@gmail.com>
2019-03-22 13:42:09 +0100

    Add error message

--- src/index.ts
+++ src/index.ts
@@ -37,5 +37,45 @@ createConnection()
 
     console.log("Save user...");
     await user.save();
+
+    /**
+    
+    {
+        QueryFailedError: duplicate key value violates unique constraint "PK_7b79aec4c8b4b2b936306016964"
+            at new QueryFailedError (/Users/Adam/Projects/typeorm-one-to-many-insert/src/error/QueryFailedError.ts:9:9)
+            at Query.callback (/Users/Adam/Projects/typeorm-one-to-many-insert/src/driver/postgres/PostgresQueryRunner.ts:176:30)
+            at Query.handleError (/Users/Adam/Projects/typeorm-one-to-many-insert/node_modules/pg/lib/query.js:142:17)
+            at Connection.connectedErrorMessageHandler (/Users/Adam/Projects/typeorm-one-to-many-insert/node_modules/pg/lib/client.js:183:17)
+            at Connection.emit (events.js:197:13)
+            at Connection.EventEmitter.emit (domain.js:481:20)
+            at Socket.<anonymous> (/Users/Adam/Projects/typeorm-one-to-many-insert/node_modules/pg/lib/connection.js:125:12)
+            at Socket.emit (events.js:197:13)
+            at Socket.EventEmitter.emit (domain.js:481:20)
+            at addChunk (_stream_readable.js:288:12)
+        message: 'duplicate key value violates unique constraint "PK_7b79aec4c8b4b2b936306016964"',
+        name: 'QueryFailedError',
+        length: 241,
+        severity: 'ERROR',
+        code: '23505',
+        detail: 'Key ("postId", "userId")=(1, 1) already exists.',
+        hint: undefined,
+        position: undefined,
+        internalPosition: undefined,
+        internalQuery: undefined,
+        where: undefined,
+        schema: 'public',
+        table: 'comment',
+        column: undefined,
+        dataType: undefined,
+        constraint: 'PK_7b79aec4c8b4b2b936306016964',
+        file: 'nbtinsert.c',
+        line: '535',
+        routine: '_bt_check_unique',
+        query:
+        'INSERT INTO "comment"("postId", "userId", "text") VALUES ($1, $2, $3)',
+        parameters: [ 1, 1, 'Timbers comment' ]
+    }
+    
+     */
   })
   .catch(error => console.log(error));
